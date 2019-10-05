81523c5bc6d7d37f8b5ade3d2790179f81b71cc5
Huseph <920993863@qq.com>
2019-10-05 22:18:06 +0800

    update of today, again!(beacause today is a hard-working day)

--- test1.py
+++ test1.py
@@ -1,8 +1,16 @@
-data = {}
-while True:
-    content = input('?????')
-    if content == '!p':
-        break
-    data['a'] = content
-    print(type(data['a']))
-    del data['a']
+import urllib.request
+import os
+
+url1 = 'https://worldcosplay.net/member/Itsuki-chan/characters/'
+
+
+def url_open(url):
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36')
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    return html
+
+url = 'https://worldcosplay.net/photo/6859679'
+print(url_open(url))
+
