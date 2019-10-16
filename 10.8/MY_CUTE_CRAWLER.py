44351859620202c4340c49958f9943544540cd2e
Huseph <920993863@qq.com>
2019-10-16 16:51:24 +0800

    fix some bugs in my crawler

--- 10.8/MY_CUTE_CRAWLER.py
+++ 10.8/MY_CUTE_CRAWLER.py
@@ -10,36 +10,48 @@ url1 = 'https://worldcosplay.net/member/Itsuki-chan/characters/'
 
 
 def url_open(url):
-    print('这是url::',url,'\n')
+    print('这是url::',url)
     req = urllib.request.Request(url)
     req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 YaBrowser/19.9.3.314 Yowser/2.5 Safari/537.36')
-    response = urllib.request.urlopen(url)
+    
+    print("req.add_header succeeded")
+    
+    response = urllib.request.urlopen(req)
+    
+    print("urlopen succeeded")
+    
     html = response.read()
+    print("read succeeded")
     return html
     
 
 def get_img(img_num):
+    img_name = str(img_num) + '.jpg'
+    if os.path.exists(img_name):
+        print(img_name, ' exist! skipping...')
+        return
     url_aim = 'https://worldcosplay.net/photo/' + str(img_num)
     html = str(url_open(url_aim), encoding = 'utf-8')
     img_num1 = 0
     img_num1 = int(html.find('"image" src="', img_num1)) + 13
     img_num2 = int(html.find('"></div>\n\t\t\t</div>\n\t\t\t', img_num1))
     #print('我在get_img里：', type(img_num1), img_num1,type(img_num2), img_num2)
-    url_img = html[img_num1: img_num2]
-    img = url_open(url_img)
-    img_name = str(img_num) + '.jpg'
-    with open(img_name, 'wb') as f:
-        f.write(img)
-        
+    if img_num2 > 0 :
+        url_img = html[img_num1: img_num2]
+        img = url_open(url_img)
+        with open(img_name, 'wb') as f:
+            f.write(img)
+            
 
 def character(ch_num, ch_name):
+    ch_name = str(ch_num) + ch_name;
     if not os.path.exists(ch_name):
         os.mkdir(ch_name)
     os.chdir(ch_name)
-    print(type(ch_num))
+    #print(type(ch_num))
     url_ch = 'https://worldcosplay.net/api/member/126641/characters/' + ch_num + '/photos.json?limit=16&p3_photo_list=true&page='
     html = ''
-    for i in range(1,5):
+    for i in range(1,6):
         html += str(url_open(url_ch + str(i)), encoding = 'utf-8')
     #print('这是分页的代码！！:',html)
     img_num1 = 0
